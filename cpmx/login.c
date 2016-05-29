a16a8355b2cb92f8b175880abc65397d062ab93e
Miguel Garcia <miguelvisgarcia@gmail.com>
2016-05-29 22:20:49 +0200

    cpmx: login command reworked

--- cpmx/login.c
+++ cpmx/login.c
@@ -1,57 +1,100 @@
+/*	login.c
+
+	Unix-like login for CP/M.
+
+	Not too much useful work here yet.
+
+	Copyright (c) Miguel I. Garcia Lopez.
+
+	29 May 2016 : 1.00 : 1st version.
+*/
+
+/* Defines for MESCC libraries
+   ---------------------------
+*/
+#define CC_NO_DIV       // Exclude support for DIVISION and MODULUS.
+
+/* Standard MESCC library
+   ----------------------
+*/
 #include <mescc.h>
+
+/* Standard MESCC libraries
+   ------------------------
+*/
 #include <conio.h>
-#include <printf.h>
 #include <string.h>
 
+/* Project defs.
+   -------------
+*/
+#define APP_NAME    "login"
+#define APP_VERSION "v1.00 / 29 May 2016"
+#define APP_COPYRGT "(c) 2016 FloppySoftware"
+#define APP_USAGE   "login"
+
+#define USERNAME "elvis"
+#define PASSWORD "presley"
+
+#define NAME_MAX 8
+#define NAME_SIZ 9
+
+#define PASS_MAX 8
+#define PASS_SIZ 9
+
+/* The program
+   -----------
+*/
 main(argc, argv)
 int argc, argv[];
 {
-	printf("Wellcome to CPMX\n\n");
-
-	LogIn();
-}
-
-#define MAX_USRNAME 9
-#define MAX_PASS 9
-
-int LogIn()
-{
-	char usrname[MAX_USRNAME], usrpass[MAX_PASS];
+	char usrname[NAME_SIZ],
+	     usrpass[NAME_SIZ];
 	int i;
 
 	while(1)
 	{
-		usrname[0]=usrpass[0]=0;
+		// Ask for user name
+		putstr("Login: ");
+		read_line(usrname, NAME_MAX, 0);
 
-		printf("User: ");
-		ReadLine(usrname, MAX_USRNAME-1); putchar('\n');
-		printf("Password: ");
-		ReadLine(usrpass, MAX_PASS-1); putchar('\n');
+		// Ask for password
+		putstr("Password: ");
+		read_line(usrpass, PASS_MAX, 1);
 
-		if(!strcmp(usrname, "elvis") && !strcmp(usrpass, "presley"))
+		// Check if username and password are valid
+		if(!strcmp(usrname, USERNAME) && !strcmp(usrpass, PASSWORD)) {
+
+			// Username and password are valid
 			return 0;
+		}
 
-		printf("\nBad user name or password. Please, retry.\n\n");
+		// Error, try again
+		putstr("\nBad user name or password.\n\n");
 	}
-
-	return -1;
 }
 
-
-ReadLine(buf, width) /* from newccp.c */
+/* Read line from console
+   ----------------------
+*/
+read_line(buf, maxlen, pass)
 char *buf;
-int width;
+int maxlen, pass;
 {
 	int len;
 	char ch;
 
-	printf("%s", buf); len=strlen(buf);
+	// Buffer is empty
+	len = 0;
 
+	// Read some characters from console
 	while(1)
 	{
-		switch(ch=getch())
+		switch(ch = getch())
 		{
-			case 8 :
+			case 8   :
+			case 127 :
+				// Delete last character
 				if(len)
 				{
 					putchar(8); putchar(' '); putchar(8);
@@ -59,11 +102,19 @@ int width;
 				}
 				break;
 			case '\r' :
-				buf[len]=0;
+			case '\n' :
+				// Done
+				buf[len] = 0;
+				putchar('\n');
 				return;
 			default :
-				if(ch >= ' ' && len<width)
-					putchar(buf[len++]=ch);
+				// Add character
+				if(ch >= ' ' && len < maxlen) {
+					buf[len++] = ch;
+
+					putchar(pass ? '*' : ch);
+				}
+				break;
 		}
 	}
 }
