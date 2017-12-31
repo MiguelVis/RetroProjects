948a4d5fe236f52d9c04a08430f8d7f32bdd592d
Miguel Garcia <miguelvisgarcia@gmail.com>
2017-12-31 17:21:40 +0100

    te: updated copyright notice

--- te/te.c
+++ te/te.c
@@ -4,7 +4,7 @@
 
 	Main module.
 
-	Copyright (c) 2015-2016 Miguel Garcia / FloppySoftware
+	Copyright (c) 2015-2017 Miguel Garcia / FloppySoftware
 
 	This program is free software; you can redistribute it and/or modify it
 	under the terms of the GNU General Public License as published by the
