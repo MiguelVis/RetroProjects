b5931d0d9491795f56fc19e934ea65e31481c25b
Miguel Garcia <miguelvisgarcia@gmail.com>
2018-12-18 20:53:24 +0100

    mescc: update template with better descriptions.

--- mescc/template.c
+++ mescc/template.c
@@ -1,8 +1,9 @@
-/*	template.c
+/*	$template.c
 
-	Short description.
+	Short description of this project. Replace $template in the contents of this file,
+	with the real name of your program.
 
-	Copyright (c) 2016 Miguel Garcia / FloppySoftware
+	Copyright (c) 2016-2018 Miguel Garcia / FloppySoftware
 
 	This program is free software; you can redistribute it and/or modify it
 	under the terms of the GNU General Public License as published by the
@@ -24,16 +25,16 @@
 		cpm-connections.blogspot.com
 		floppysoftware@gmail.com
 
-	To compile with MESCC:
+	To compile with MESCC under CP/M:
 
-		cc template
-		ccopt template
-		zsm template
-		hextocom template
+		cc $template
+		ccopt $template
+		zsm $template
+		hextocom $template
 
 	Usage:
 
-		template [-options...] [filename...]
+		$template [-options...] [filename...]
 
 	Revisions:
 
@@ -42,30 +43,31 @@
 		20 Jul 2016 : Remove CC_FILEIO_SMALL. Added CC_FOPEN_A, CC_FREAD, CC_FWRITE, CC_FGETS.
 		11 Nov 2016 : Update GPL license to v3.
 		30 Nov 2016 : Add APP_INFO. Update usage(). Improve command line parsing.
+		18 Dec 2018 : Better descriptions.
 */
 
 /* Defines for MESCC libraries
    ---------------------------
    Leave only what you need.
 */
-#define CC_STDIO        // Support for stdin, stdout, stderr.
-#define CC_REDIR        // Support for command line redirection - needs CC_STDIO.
+#define CC_STDIO      // Support for stdin, stdout, stderr.
+#define CC_REDIR      // Support for command line redirection - needs CC_STDIO.
 
-#define CC_CONIO_BIOS   // Console I/O through BIOS instead of BDOS.
+#define CC_CONIO_BIOS // Console I/O through BIOS instead of BDOS.
 
-#define CC_FCX          // Support for user number in filenames.
-#define CC_FCX_DIR      // Support for named user numbers - needs CC_FCX and DirToDrvUsr().
+#define CC_FCX        // Support for user number in filenames - ie: "a01:letter.doc".
+#define CC_FCX_DIR    // Support for named directories (aliases) - needs CC_FCX and DirToDrvUsr() - ie: "docs:letter.doc".
 
-#define CC_FOPEN_A	// Enable modes "a" and "ab" for fopen.
-#define CC_FREAD	// Include fread().
-#define CC_FWRITE	// Include fwrite().
-#define CC_FGETS	// Include fgets().
+#define CC_FOPEN_A    // Enable modes "a" and "ab" for fopen().
+#define CC_FREAD      // Include fread().
+#define CC_FWRITE     // Include fwrite().
+#define CC_FGETS      // Include fgets().
 
-#define CC_NO_MUL       // Exclude support for MULTIPLICATION.
-#define CC_NO_DIV       // Exclude support for DIVISION and MODULUS.
-#define CC_NO_SWITCH    // Exclude support for SWITCH.
+#define CC_NO_MUL     // Exclude support for MULTIPLICATION.
+#define CC_NO_DIV     // Exclude support for DIVISION and MODULUS.
+#define CC_NO_SWITCH  // Exclude support for SWITCH.
 
-#define CC_NO_ARGS      // Exclude support for ARGC and ARGV.
+#define CC_NO_ARGS    // Exclude support for ARGC and ARGV in main().
 
 /* Standard MESCC library
    ----------------------
@@ -104,16 +106,16 @@
 /* Project libraries
    -----------------
 */
-#include "template.h"
+#include "$template.h"
 
 /* Project defs.
    -------------
 */
-#define APP_NAME    "template"
+#define APP_NAME    "$template"
 #define APP_VERSION "v1.00 / 06 Jan 2016"
 #define APP_COPYRGT "(c) 2016 FloppySoftware"
 #define APP_INFO    "This program does something."
-#define APP_USAGE   "template [-option...] [filename...]"
+#define APP_USAGE   "$template [-option...] [filename...]"
 
 /* Program entry
    -------------
@@ -122,15 +124,14 @@ main(argc, argv)
 int argc;
 unsigned int argv[]; // char *argv[] - unsupported by MESCC (yet?)
 {
-	char *pch; int i, inopt;
+	char *pch;
+	int i, n, inopt;
 
 	// Show usage if there are no arguments
-
 	if(argc == 1)
 		usage();
 
 	// Check arguments in command line
-
 	for(i = 1; i < argc; ++i)
 	{
 		pch = argv[i];
@@ -138,7 +139,6 @@ unsigned int argv[]; // char *argv[] - unsupported by MESCC (yet?)
 		if(*pch == '-' && pch[1])
 		{
 			// Option/s
-
 			inopt = 1;
 
 			while(inopt && *(++pch))
@@ -152,33 +152,35 @@ unsigned int argv[]; // char *argv[] - unsupported by MESCC (yet?)
 
 					// -n256
 					case 'N' :
-						if(*(++pch))
+						if(isdigit(*(++pch)))
 						{
 							n = atoi(pch);
+							inopt = 0;
 						}
 						else
 						{
 							error("Bad argument");
 						}
 						break;
-					...
 					default  :
 						error("Bad option");
 						break;
 				}
 			}
 		}
+		else
+		{
+			break;
+		}
 	}
 
 	// Check filenames in command line
-
 	if(i < argc)
 	{
 		// First filename is argv[i]
 	}
 
 	// Success
-
 	exit(0);
 }
 
