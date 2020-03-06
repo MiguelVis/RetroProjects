94c93dad95a1a1165c8da2370c7e26c96004af50
Miguel Garcia <miguelvisgarcia@gmail.com>
2020-03-06 22:08:23 +0100

    samarux: has been moved to its own repository.

--- samarux/make_ext2.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem Make a SamaruX external command from a built-in one.
-rem Usage: make_ext2 sx_name name
-rem IE:    make_ext2 sx_cat cat
-call make_ext %1
-if exist %2.x del %2.x
-ren %1.x %2.x
